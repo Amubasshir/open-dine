935ed0658ef094bff1abb28b5f31141b5ae1dad8
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-12 21:57:47 +0600

    mounted issue solved

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,47 +1,28 @@
-// 'use client';
-
-// import { useRouter } from 'next/router';
-// import { useState } from 'react';
-
-// const SearchBar = () => {
-//   const router = useRouter();
-//   const [location, setLocation] = useState('');
-
-//   return (
-//     <div className="m-auto mt-2 flex justify-center py-3 text-left">
-//       <input
-//         className="mr-3 w-[450px] rounded p-2 text-lg outline-none "
-//         type="text"
-//         placeholder="location, restaurant, cousine"
-//         value={location}
-//         onChange={(e) => setLocation(e.target.value)}
-//       />
-//       <button
-//         className="rounded bg-red-600 px-8 py-2 font-medium text-white "
-//         onClick={() => {
-//           if (location === 'banana') return;
-//           router.push('/search');
-//         }}
-//       >
-//         Lets go
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SearchBar;
-
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 const SearchBar = () => {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
+
   return (
     <div className="m-auto mt-2 flex justify-center py-3 text-left">
       <input
         className="mr-3 w-[450px] rounded p-2 text-lg outline-none "
         type="text"
         placeholder="location, restaurant, cousine"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
       />
-      <button className="rounded bg-red-600 px-8 py-2 font-medium text-white ">
+      <button
+        className="rounded bg-red-600 px-8 py-2 font-medium text-white "
+        onClick={() => {
+          if (location === 'banana') return;
+          router.push('/search');
+        }}
+      >
         Lets go
       </button>
     </div>
@@ -49,3 +30,22 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+
+// 'use client';
+
+// const SearchBar = () => {
+//   return (
+//     <div className="m-auto mt-2 flex justify-center py-3 text-left">
+//       <input
+//         className="mr-3 w-[450px] rounded p-2 text-lg outline-none "
+//         type="text"
+//         placeholder="location, restaurant, cousine"
+//       />
+//       <button className="rounded bg-red-600 px-8 py-2 font-medium text-white ">
+//         Lets go
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
